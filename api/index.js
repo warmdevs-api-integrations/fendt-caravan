bf2336ee4f64209d13a01df008237e485fa2f595
Anna Morozevych <ann@warmdevs.com>
2024-04-24 09:25:45 +0300

    vercel.json was modified

--- api/index.js
+++ api/index.js
@@ -3,12 +3,13 @@ const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
 export default async (request, result) => {
   const { body } = request;
+  let key;
 
   try {
     let resultData = await lib.http.request['@1.1.7'].get({
       url: `https://api.zipcodestack.com/v1/distance`,
       headers: {
-        'apikey': `01HS1BJ74CVQY0XY5C5HV59NH2`,
+        'apikey': '01HS1BJ74CVQY0XY5C5HV59NH2',
         'Accept': `application/json`
       },
       queryParams: {
@@ -22,12 +23,13 @@ export default async (request, result) => {
     const recivedData = resultData.data.results;
 
     if (Object.keys(recivedData).length > 0) {
-      let key = Object.keys(recivedData).reduce((key, v) => recivedData[v] < recivedData[key] ? v : key);
-      return new Response(key);
+      key = Object.keys(recivedData).reduce((key, v) => recivedData[v] < recivedData[key] ? v : key);
     } else {
-      return new Response('');
+      key = '';
     }
   } catch (error) {
-    return new Response('NOT FOUND')
+    return result.json('no key');
   }
+
+  return result.json(key);
 }
